9e4ac54e1e2e63d026272d9e7994bc11010a0624
Eric Katz <ericpkatz@gmail.com>
2015-02-12 00:46:25 +0000

    some tests for calculator

---
+++ test/calculator_spec.js
@@ -0,0 +1,37 @@
+var Calculator = {
+    foo: "bar",
+    add: function(a, b){
+        return a + b;
+    },
+    subtract: function(a, b){
+       return a - b; 
+    }
+};
+describe("Calculator", function(){
+   it("Calculator is defined", function(){
+     expect(Calculator).toBeDefined();  
+   });
+   
+   describe("addition", function(){
+      it("can add 1 and 1", function(){
+        expect(Calculator.add(1,1)).toEqual(2);  
+      }); 
+      
+      it("can add 2 and 2", function(){
+        expect(Calculator.add(2,2)).toEqual(4);  
+      }); 
+   });
+    describe("subtraction", function(){
+        it("is defined", function(){
+           expect(Calculator.subtract).toBeDefined(); 
+        }); 
+       it("1 -1 is 0", function(){
+           expect(Calculator.subtract(1,1)).toEqual(0);
+       }); 
+       
+       it("10 - 3 is 7", function(){
+           expect(Calculator.subtract(10,3)).toEqual(7);
+       }); 
+    }); 
+   
+});
